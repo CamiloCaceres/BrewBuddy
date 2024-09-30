7c37b92c910c589679ecfcf78d784c6df7eecc19
Camilo Caceres <camiloac97@gmail.com>
2024-09-30 15:37:58 -0500

    feat: add types

--- types/index.ts
+++ types/index.ts
@@ -32,3 +32,60 @@ export interface Entry {
     }[];
     images: string[];
   }
+export type Unit = 'ml' | 'liters' | 'cups' | 'ounces' | 'grams';
+
+interface IngredientWithAmount {
+  amount: number;
+  unit: Unit;
+}
+
+export interface Ingredient {
+  name: string;
+  amount: number;
+  unit: Unit;
+}
+
+export interface FlavorProfile {
+  sweetness: number;
+  acidity: number;
+  bitterness: number;
+  carbonation: number;
+}
+
+export interface Ratings {
+  averageRating: number;
+  numberOfRatings: number;
+}
+
+export interface KombuchaRecipe {
+  id: number;
+  name: string;
+  description: string;
+  teaType: string;
+  sugar: IngredientWithAmount;
+  sugarType: string;
+  F2ingredients: Ingredient[];
+  instructions: {
+    brewing: string;
+    firstFermentation: string;
+    secondFermentation: string;
+  };
+  F1Days: number;
+  F2Days: number;
+  water: IngredientWithAmount;
+  tea: IngredientWithAmount;
+  starter: IngredientWithAmount;
+  flavorProfile: FlavorProfile;
+  tags: string[];
+  createdAt: Date;
+  updatedAt: Date;
+  ratings: Ratings;
+  author: string;
+  isPublic: boolean;
+}
+export interface BakersPercentageRecipe {
+  water: number;  // Always 100%
+  sugar: number;
+  tea: number;
+  starter: number;
+}

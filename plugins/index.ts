b56fb2c880a3bd4dec690731232e77a4144b6bfd
Camilo Caceres <camiloac97@gmail.com>
2024-10-18 19:06:16 -0500

    feat: add autoanimate

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,5 +1,7 @@
 import { VueQueryPlugin } from '@tanstack/vue-query'
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 
 export default defineNuxtPlugin((app) => {
   app.vueApp.use(VueQueryPlugin)
+  app.vueApp.use(autoAnimatePlugin)
 })

08f683eb68a76df3fa49d327f4771cdeb2a6d488
Camilo Caceres <camiloac97@gmail.com>
2024-10-18 19:24:42 -0500

    feat: add v-calendar

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,7 +1,10 @@
 import { VueQueryPlugin } from '@tanstack/vue-query'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+import VCalendar from 'v-calendar';
+import 'v-calendar/style.css';
 
 export default defineNuxtPlugin((app) => {
   app.vueApp.use(VueQueryPlugin)
   app.vueApp.use(autoAnimatePlugin)
+  app.vueApp.use(VCalendar, {})
 })

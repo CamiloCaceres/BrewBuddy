619292ff94c9d8bb77a280e290e2b44f3f059fd0
Camilo Caceres <camiloac97@gmail.com>
2024-10-19 01:36:04 -0500

    refactor: move unreused logic to component

--- utils/batch.ts
+++ utils/batch.ts
@@ -1,19 +1,3 @@
-export const getStatusColor = (batchStatus: string) => {
-  switch (batchStatus) {
-    case "pending":
-      return "yellow";
-    case "firstFermentation":
-      return "orange";
-    case "secondFermentation":
-      return "blue";
-    case "completed":
-      return "green";
-    case "discarded":
-      return "red";
-    default:
-      return "gray";
-  }
-};
 export const getCurrentStageName = (batchStatus: string) => {
   switch (batchStatus) {
     case "pending":

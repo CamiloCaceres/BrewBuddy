b06d6bbf9217252282c3c1359f578f6540ece303
Camilo Caceres <camiloac97@gmail.com>
2024-10-19 00:35:29 -0500

    feat: when starting fermentation, set start date and expected f1 and f2 days

--- utils/batch.ts
+++ utils/batch.ts
@@ -1,45 +1,51 @@
 export const getStatusColor = (batchStatus: string) => {
-    switch (batchStatus) {
-      case "pending":
-        return "yellow";
-      case "firstFermentation":
-        return "orange";
-      case "secondFermentation":
-        return "blue";
-      case "completed":
-        return "green";
-      case "discarded":
-        return "red";
-      default:
-        return "gray";
-    }
-  };
-  export const getCurrentStageName = (batchStatus: string) => {
-    switch (batchStatus) {
-      case "pending":
-        return "Batch Created";
-      case "firstFermentation":
-        return "First Fermentation (F1)";
-      case "secondFermentation":
-        return "Second Fermentation (F2)";
-      case "completed":
-        return "Completed";
-      case "discarded":
-        return "Discarded";
-      default:
-        return "Unknown";
-    }
-  };
-  // Computes the next stage button name based on the current stage of fermentation.
+  switch (batchStatus) {
+    case "pending":
+      return "yellow";
+    case "firstFermentation":
+      return "orange";
+    case "secondFermentation":
+      return "blue";
+    case "completed":
+      return "green";
+    case "discarded":
+      return "red";
+    default:
+      return "gray";
+  }
+};
+export const getCurrentStageName = (batchStatus: string) => {
+  switch (batchStatus) {
+    case "pending":
+      return "Batch Created";
+    case "firstFermentation":
+      return "First Fermentation (F1)";
+    case "secondFermentation":
+      return "Second Fermentation (F2)";
+    case "completed":
+      return "Completed";
+    case "discarded":
+      return "Discarded";
+    default:
+      return "Unknown";
+  }
+};
+// Computes the next stage button name based on the current stage of fermentation.
 export const getNextStageName = (batchStatus: string) => {
-    switch (batchStatus) {
-      case "pending":
-        return "Start First Fermentation";
-      case "firstFermentation":
-        return "Start Second Fermentation";
-      case "secondFermentation":
-        return "Complete Fermentation";
-      default:
-        return "Unknown Stage";
-    }
-  };
+  switch (batchStatus) {
+    case "pending":
+      return "Start First Fermentation";
+    case "firstFermentation":
+      return "Start Second Fermentation";
+    case "secondFermentation":
+      return "Complete Fermentation";
+    default:
+      return "Unknown Stage";
+  }
+};
+
+export const calculateExpectedEndDate = (startDate: Date, fermentationDays: number) => {
+  const expectedEndDate = new Date(startDate);
+  expectedEndDate.setDate(startDate.getDate() + fermentationDays);
+  return expectedEndDate;
+};

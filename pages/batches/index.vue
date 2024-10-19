343424a59f33a29781338e1b8d1955b3498ececd
Camilo Caceres <camiloac97@gmail.com>
2024-10-19 04:04:19 -0500

    feat: list all batches

--- pages/batches/index.vue
+++ pages/batches/index.vue
@@ -1,11 +1,33 @@
 <template>
-  <div>
-    <h1>Batches</h1>
+  <div v-if="isPending">
+    <UProgress indeterminate />
+  </div>
+  <div v-else-if="isError">
+    <UAlert color="red" title="Error">
+      {{ error }}
+    </UAlert>
+  </div>
+  <div v-else>
+    <h1 class="text-2xl font-bold py-4">Batches</h1>
+    <div class="space-y-4">
+      <BatchCard  v-for="batch in batches" :key="batch.id" :batch="batch" />
+    </div>
     <UButton to="/batches/create">Create Batch</UButton>
     <UButton to="/batches/view-batch">View Batch</UButton>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { useQuery } from "@tanstack/vue-query";
+const { getAllBatches } = usePocketBase();
 
+const {
+  isPending,
+  isError,
+  data: batches,
+  error,
+} = useQuery({
+  queryKey: ["batches"],
+  queryFn: () => getAllBatches(),
+});
 </script>

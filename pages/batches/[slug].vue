78b31bdd2b67436c6efb3993c95488f4cd88c81a
Camilo Caceres <camiloac97@gmail.com>
2024-10-19 00:35:13 -0500

    feat: when starting fermentation, set start date and expected f1 and f2 days

--- pages/batches/[slug].vue
+++ pages/batches/[slug].vue
@@ -20,30 +20,85 @@
       </template>
       <div class="space-y-4">
         <p>
-          <strong>Current Day:</strong>
+          Current Day:
           {{ currentFermentationDay ? currentFermentationDay : "0" }}
         </p>
         <UProgress :value="fermentationProgress" color="primary" />
         <p>{{ fermentationProgressText }}</p>
 
         <div class="flex space-x-2">
-            <UButton v-if="batch.stage !== 'discarded'" icon="i-heroicons-trash" color="red" variant="outline" :loading="isDiscardingBatch" @click="discardBatch">Discard Batch</UButton>
-          <UButton :loading="isUpdatingBatch" @click="moveToNextStage">{{ getNextStageName }}</UButton>
+          <UButton
+            v-if="batch.stage !== 'discarded' && batch.stage !== 'completed'"
+            icon="i-heroicons-trash"
+            color="red"
+            variant="outline"
+            :loading="isDiscardingBatch"
+            @click="discardBatch"
+            >Discard Batch</UButton
+          >
+          <UButton
+            v-if="batch.stage !== 'completed'"
+            :loading="isUpdatingBatch"
+            @click="moveToNextStage"
+            >{{ getNextStageName(batch.stage) }}</UButton
+          >
         </div>
       </div>
     </UCard>
     <!-- TODO: if stage is pending, add a calendar with the expected fermentation days,  -->
+
+    <UCard>
+      <template #header>
+        <h3 class="text-lg font-semibold">Batch Information</h3>
+      </template>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div>
+          <p>
+            Start Date:
+            {{ batch.start_date ? batch.start_date : "Start Date Not Set" }}
+          </p>
+          <p class="capitalize">Batch Type: {{ batch.brew_type }}</p>
+          <p>Tracking Vessel: {{ batch.is_tracking_vessel ? "Yes" : "No" }}</p>
+          <div v-if="batch.is_tracking_vessel">
+            <p>First Fermentation Vessel: {{ batch.f1_vessel }}</p>
+            <p>Second Fermentation Vessel: {{ batch.f2_vessel }}</p>
+          </div>
+        </div>
+        <div>
+          <p>
+            Tracking Temperature:
+            {{ batch.is_tracking_temperature ? "Yes" : "No" }}
+          </p>
+          <p>Tracking pH: {{ batch.is_tracking_PH ? "Yes" : "No" }}</p>
+          <div v-if="batch.start_date">
+            <p>
+              Expected F1 End Date:
+              {{ batch.expected_f1_end }}
+            </p>
+            <p>
+              Expected F2 End Date:
+              {{ batch.expected_f2_end }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </UCard>
   </div>
 </template>
 
 <script setup>
-import { useQuery, useMutation, useQueryClient } from '@tanstack/vue-query'
+import { useQuery, useMutation, useQueryClient } from "@tanstack/vue-query";
 const { slug } = useRoute().params;
 const { getBatchBySlug, updateBatch } = usePocketBase();
 
-const queryClient = useQueryClient()
+const queryClient = useQueryClient();
 
-const { isLoading, isError, data: batch, error } = useQuery({
+const {
+  isLoading,
+  isError,
+  data: batch,
+  error,
+} = useQuery({
   queryKey: ["batch", slug],
   queryFn: () => getBatchBySlug(slug),
 });
@@ -51,14 +106,14 @@ const { isLoading, isError, data: batch, error } = useQuery({
 const { mutate: updateBatchMutation } = useMutation({
   mutationFn: (newBatch) => updateBatch(batch.value.id, newBatch),
   onSuccess: (data) => {
-    queryClient.setQueryData(["batch", slug], data)
+    queryClient.setQueryData(["batch", slug], data);
     isUpdatingBatch.value = false;
     isDiscardingBatch.value = false;
   },
   onError: () => {
     isUpdatingBatch.value = false;
   },
-})
+});
 
 //this function calculates the current fermentation day based on the start date and the current date
 const currentFermentationDay = computed(() => {
@@ -70,7 +125,7 @@ const currentFermentationDay = computed(() => {
 });
 // Computes the fermentation progress as a percentage based on the current fermentation day and total fermentation days.
 const fermentationProgress = computed(() => {
-  if (!batch.value?.startDate || !batch.value?.expand?.recipe) {
+  if (!batch.value?.start_date || !batch.value?.expand?.recipe) {
     return 0;
   }
   const totalDays =
@@ -85,13 +140,16 @@ const fermentationProgressText = computed(() => {
     return "Loading...";
   }
   if (batch.value.stage === "firstFermentation") {
-    return `Day ${currentFermentationDay.value} of ${batch.value.expand.recipe.F1Days || 0} (First Fermentation)`;
+    return `Day ${currentFermentationDay.value} of ${
+      batch.value.expand.recipe.F1Days || 0
+    } (First Fermentation)`;
   } else if (batch.value.stage === "secondFermentation") {
     const secondFermentationDay =
-      currentFermentationDay.value -
-      (batch.value.expand.recipe.F1Days || 0);
-    return `Day ${secondFermentationDay} of ${batch.value.expand.recipe.F2Days || 0} (Second Fermentation)`;
-  } else if(batch.value.stage === "pending") {
+      currentFermentationDay.value - (batch.value.expand.recipe.F1Days || 0);
+    return `Day ${secondFermentationDay} of ${
+      batch.value.expand.recipe.F2Days || 0
+    } (Second Fermentation)`;
+  } else if (batch.value.stage === "pending") {
     return "Pending to start fermentation";
   } else {
     return "Fermentation Complete";
@@ -105,7 +163,18 @@ const moveToNextStage = () => {
   isUpdatingBatch.value = true;
   switch (batch.value.stage) {
     case "pending":
-    updateBatchMutation({ stage: "firstFermentation" });
+      updateBatchMutation({
+        stage: "firstFermentation",
+        start_date: new Date().toISOString(),
+        expected_f1_end: calculateExpectedEndDate(
+          new Date(),
+          batch.value.expand.recipe.F1Days || 7
+        ).toISOString(),
+        expected_f2_end: calculateExpectedEndDate(
+          new Date(),
+          batch.value.expand.recipe.F2Days || 3
+        ).toISOString(),
+      });
       break;
     case "firstFermentation":
       updateBatchMutation({ stage: "secondFermentation" });
@@ -114,12 +183,11 @@ const moveToNextStage = () => {
       updateBatchMutation({ stage: "completed" });
       break;
   }
-}
+};
 
 const isDiscardingBatch = ref(false);
 const discardBatch = () => {
   isDiscardingBatch.value = true;
   updateBatchMutation({ stage: "discarded" });
-}
-
+};
 </script>

bb8b7e2bb4ee813835a9ed14ff53dbd8b2f84223
Camilo Caceres <camiloac97@gmail.com>
2024-10-20 13:37:23 -0500

    feat: add filter batches button

--- pages/batches/index.vue
+++ pages/batches/index.vue
@@ -8,18 +8,71 @@
     </UAlert>
   </div>
   <div v-else>
-    <h1 class="text-2xl font-bold py-4">Batches</h1>
-    <div class="space-y-4">
-      <BatchCard  v-for="batch in batches" :key="batch.id" :batch="batch" />
+    <h1 class="text-4xl font-bold my-6">Batches</h1>
+    <div class="flex flex-wrap items-center gap-4 mb-8">
+      <UInput
+        v-model="searchQuery"
+        icon="i-heroicons-magnifying-glass"
+        placeholder="Search batches"
+        class="flex-grow max-w-md"
+      />
+      <USelect
+        v-model="selectedFilter"
+        :options="filterOptions"
+        placeholder="Filter batches"
+        icon="i-heroicons-adjustments-horizontal-20-solid"
+        class="w-48"
+      />
+      <UButton
+        icon="i-heroicons-plus"
+        to="/batches/create"
+        class="sm:ml-auto"
+      >
+        Create Batch
+      </UButton>
     </div>
-    <UButton to="/batches/create">Create Batch</UButton>
-    <UButton to="/batches/view-batch">View Batch</UButton>
+      <h1 class="text-2xl font-bold py-4"> {{ filterOptions.find(option => option.value === selectedFilter)?.label }}</h1>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <BatchCard
+        v-for="batch in filteredBatches"
+        :key="batch.id"
+        :batch="batch"
+      />
+    </div>
+   
   </div>
 </template>
 
 <script setup lang="ts">
 import { useQuery } from "@tanstack/vue-query";
+import { computed } from "vue";
 const { getAllBatches } = usePocketBase();
+
+
+const searchQuery = ref('')
+
+const selectedFilter = ref('all')
+
+const filteredBatches = computed(() => {
+  if (selectedFilter.value === 'all') return batches.value
+  else if (selectedFilter.value === 'active') return batches.value?.filter(batch => batch.stage !== 'completed' && batch.stage !== 'discarded')
+  return batches.value?.filter(batch => batch.stage === selectedFilter.value)
+
+})
+
+const filterOptions = [
+  { label: 'All Batches', value: 'all' },
+  { label: 'Active Batches', value: 'active' },
+  { label: 'Fermentation 1', value: 'firstFermentation' },
+  { label: 'Fermentation 2', value: 'secondFermentation' },
+  { label: 'Discarded Batches', value: 'discarded' },
+  { label: 'Completed Batches', value: 'completed' }
+]
+
+watch(selectedFilter, (newValue) => {
+  console.log(`Filter changed to: ${newValue}`)
+  // Implement your filtering logic here
+})
 
 const {
   isPending,
@@ -30,4 +83,11 @@ const {
   queryKey: ["batches"],
   queryFn: () => getAllBatches(),
 });
+
+const activeBatches = computed(
+  () =>
+    batches.value?.filter(
+      (batch) => batch.stage !== "completed" && batch.stage !== "discarded"
+    ) || []
+);
 </script>

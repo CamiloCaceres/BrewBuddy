c443ce3075e60dd3ef24dde5ad3d0a68e90d4fa0
Camilo Caceres <camiloac97@gmail.com>
2024-10-20 13:39:33 -0500

    ui: add autoanimate

--- pages/batches/index.vue
+++ pages/batches/index.vue
@@ -32,7 +32,7 @@
       </UButton>
     </div>
       <h1 class="text-2xl font-bold py-4"> {{ filterOptions.find(option => option.value === selectedFilter)?.label }}</h1>
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div v-auto-animate class="grid grid-cols-1 md:grid-cols-2 gap-4">
       <BatchCard
         v-for="batch in filteredBatches"
         :key="batch.id"
